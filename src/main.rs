1cc332974dfdbc0c2ab426e828083e0e17f78b0e
Phil Baum <philipp.baum@mailbox.org>
2022-02-06 10:53:38 +0100

    first working version :)

--- src/main.rs
+++ src/main.rs
@@ -54,18 +54,14 @@ fn load_data(dir: &Path, todos: &mut Vec<Todo>) -> io::Result<()> {
 }
 
 fn main() {
-    let mut todos: Vec<Todo> = vec![];
-
-    load_data(Path::new("/Users/phil/TestingNotes"), &mut todos).unwrap();
-    let todo_strings: Vec<String> = todos.iter().map(|t| t.to_string()).collect();
-
-    let query: String = Input::new()
-        .with_prompt(".. where ")
-        .interact()
-        .unwrap();
+    let query: String = Input::new().with_prompt(".. where ").interact().unwrap();
 
     if query == "all" {
         loop {
+            let mut todos: Vec<Todo> = vec![];
+            load_data(Path::new("/Users/phil/TestingNotes"), &mut todos).unwrap();
+            let todo_strings: Vec<String> = todos.iter().map(|t| t.to_string()).collect();
+
             let selection = Select::with_theme(&ColorfulTheme::default())
                 .with_prompt("Choose todo to toggle:")
                 .clear(true)
@@ -74,7 +70,32 @@ fn main() {
                 .interact_opt()
                 .unwrap();
 
-            println!("Marked {} as done!", todos[selection.unwrap()].name);
+            let selected_todo = &todos[selection.unwrap()];
+            println!("Marked {} as {}!", selected_todo.name, if selected_todo.done { "open"} else { "done"});
+
+            mark_as_done(&todos[selection.unwrap()]);
         }
     }
+}
+
+fn mark_as_done(todo: &Todo) {
+    let data = fs::read_to_string(Path::new(&todo.filepath)).expect("Unable to read file");
+    let mut new_data: String = String::new();
+
+    let mut line_no = 1;
+    for line in data.lines() {
+        if line_no == todo.line_no {
+            if todo.done {
+                new_data += &line.replace("- [x]", "- [ ]");
+            } else {
+                new_data += &line.replace("- [ ]", "- [x]");
+            }
+        } else {
+            new_data += line;
+        }
+        new_data += "\n";
+        line_no += 1;
+    }
+
+    fs::write(&todo.filepath, new_data).expect("Unable to write file");
 }
