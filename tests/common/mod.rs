453a05ae1ba5b5ce7b2b5babe533023e17a94a10
Phil Baum <philipp.baum@mailbox.org>
2022-02-16 23:01:46 +0100

    Added basic integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,3 +1,27 @@
-pub fn setup() {
-    
+use std::fs::File;
+use std::io::{self, Write};
+use tempdir::TempDir;
+
+pub fn create_md_test_files_1() -> io::Result<TempDir> {
+    let dir = TempDir::new("modo_integrationtests")?;
+    create_md_file_one_open_todo(&dir, "file1.md")?;
+    Ok(dir)
+}
+
+pub fn create_md_test_files_3() -> io::Result<TempDir> {
+    let dir = TempDir::new("modo_integrationtests")?;
+    create_md_file_one_open_todo(&dir, "file1.md")?;
+    create_md_file_one_open_todo(&dir, "file2.md")?;
+    create_md_file_one_open_todo(&dir, "file3.md")?;
+    Ok(dir)
+}
+
+fn create_md_file_one_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
+    let file_path = dir.path().join(filename);
+
+    let mut f = File::create(file_path)?;
+    f.write_all(b"- [ ] A single open todo!")?;
+    f.sync_all()?;
+
+    Ok(())
 }
--- tests/md_handler_test.rs
+++ tests/md_handler_test.rs
@@ -4,9 +4,23 @@ use modo::todo::Todo;
 use modo::md_handler;
 
 #[test]
-#[should_panic]
-fn read_not_existing_path_should_fail() {
-    common::setup();
+fn should_get_one_open_todo() {
+    let temp_folder = common::create_md_test_files_1().unwrap();
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(Path::new("/This/PathDoes/n/o/t/Exist"), &mut todos).unwrap();
+    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 1);
+    assert_eq!(todos[0].name, "A single open todo!");
+    assert_eq!(todos[0].done, false);
+    temp_folder.close().unwrap();
+}
+
+#[test]
+fn should_get_three_open_todos() {
+    let temp_folder = common::create_md_test_files_3().unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 3);
+    assert_eq!(todos[0].name, "A single open todo!");
+    assert_eq!(todos[2].done, false);
+    temp_folder.close().unwrap();
 }
