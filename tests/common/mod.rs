f916fa0b1fb248591aac8d8e4c5ee90c85dc4891
Phil Baum <philipp.baum@mailbox.org>
2022-02-16 23:10:05 +0100

    refactor integration tests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,27 +1,51 @@
-use std::fs::File;
-use std::io::{self, Write};
+use std::io;
 use tempdir::TempDir;
 
 pub fn create_md_test_files_1() -> io::Result<TempDir> {
     let dir = TempDir::new("modo_integrationtests")?;
-    create_md_file_one_open_todo(&dir, "file1.md")?;
+    md_test_file_creator::one_file_one_open_todo(&dir, "file1.md")?;
+    Ok(dir)
+}
+
+pub fn create_md_test_files_2() -> io::Result<TempDir> {
+    let dir = TempDir::new("modo_integrationtests")?;
+    md_test_file_creator::one_file_one_open_todo(&dir, "file1.md")?;
+    md_test_file_creator::one_file_one_open_todo(&dir, "file2.md")?;
+    md_test_file_creator::one_file_one_open_todo(&dir, "file3.md")?;
     Ok(dir)
 }
 
 pub fn create_md_test_files_3() -> io::Result<TempDir> {
     let dir = TempDir::new("modo_integrationtests")?;
-    create_md_file_one_open_todo(&dir, "file1.md")?;
-    create_md_file_one_open_todo(&dir, "file2.md")?;
-    create_md_file_one_open_todo(&dir, "file3.md")?;
+    md_test_file_creator::one_file_five_todos_4_open(&dir, "file1.md")?;
     Ok(dir)
 }
 
-fn create_md_file_one_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
-    let file_path = dir.path().join(filename);
+pub fn create_md_test_files_4() -> io::Result<TempDir> {
+    let dir = TempDir::new("modo_integrationtests")?;
+    md_test_file_creator::one_file_five_todos_4_open(&dir, "file1.md")?;
+    md_test_file_creator::one_file_five_todos_4_open(&dir, "file2.md")?;
+    md_test_file_creator::one_file_one_open_todo(&dir, "file3.md")?;
+    Ok(dir)
+}
 
-    let mut f = File::create(file_path)?;
-    f.write_all(b"- [ ] A single open todo!")?;
-    f.sync_all()?;
+mod md_test_file_creator {
+    use std::fs::File;
+    use std::io::{self, Write};
+    use tempdir::TempDir;
 
-    Ok(())
+    pub fn one_file_one_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
+        let file_path = dir.path().join(filename);
+        let mut f = File::create(file_path)?;
+        f.write_all(b"- [ ] A single open todo!")?;
+        f.sync_all()?;
+        Ok(())
+    }
+    pub fn one_file_five_todos_4_open(dir: &TempDir, filename: &str) -> io::Result<()> {
+        let file_path = dir.path().join(filename);
+        let mut f = File::create(file_path)?;
+        f.write_all(b"- [ ] A open todo!\r\n- [ ] A open todo!\r\n- [x] A closed todo!\r\n- [ ] A open todo!\r\n- [ ] A open todo!\r\n")?;
+        f.sync_all()?;
+        Ok(())
+    }
 }
--- tests/md_handler_test.rs
+++ tests/md_handler_test.rs
@@ -4,7 +4,7 @@ use modo::todo::Todo;
 use modo::md_handler;
 
 #[test]
-fn should_get_one_open_todo() {
+fn test_single_file() {
     let temp_folder = common::create_md_test_files_1().unwrap();
     let mut todos: Vec<Todo> = vec![];
     md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
@@ -15,12 +15,36 @@ fn should_get_one_open_todo() {
 }
 
 #[test]
-fn should_get_three_open_todos() {
-    let temp_folder = common::create_md_test_files_3().unwrap();
+fn test_three_files() {
+    let temp_folder = common::create_md_test_files_2().unwrap();
     let mut todos: Vec<Todo> = vec![];
     md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 3);
     assert_eq!(todos[0].name, "A single open todo!");
     assert_eq!(todos[2].done, false);
+    temp_folder.close().unwrap();
+}
+
+#[test]
+fn test_single_files_multiple_todos() {
+    let temp_folder = common::create_md_test_files_3().unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 5);
+    assert_eq!(todos[0].name, "A open todo!");
+    assert_eq!(todos[1].done, false);
+    assert_eq!(todos[2].done, true);
+    temp_folder.close().unwrap();
+}
+
+#[test]
+fn test_multiple_files_multiple_todos() {
+    let temp_folder = common::create_md_test_files_4().unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 11);
+    assert_eq!(todos[0].name, "A open todo!");
+    assert_eq!(todos[1].done, false);
+    assert_eq!(todos[2].done, true);
     temp_folder.close().unwrap();
 }
