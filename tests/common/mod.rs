fcbb2e44b3fa9624b153f7917dc5478cd948a0d7
Phil Baum <philipp.baum@mailbox.org>
2022-02-17 22:48:13 +0100

    test headers

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -17,6 +17,12 @@ pub fn create_md_test_file_with_5_todos_4_open(file_amount: i32) -> io::Result<T
     Ok(dir)
 }
 
+pub fn create_md_test_file_with_5_todos_3_open_headers() -> io::Result<TempDir> {
+    let dir = TempDir::new("modo_integrationtests")?;
+    md_test_file_creator::one_simple_file_5_todos_3_open_with_headings(&dir, "file1.md")?;
+    Ok(dir)
+}
+
 pub fn create_folder_with_2_files_10_todos_8_open() -> io::Result<TempDir> {
     let dir = TempDir::new("modo_integrationtests")?;
     md_test_file_creator::one_simple_file_5_todos_4_open(&dir, "file1.md")?;
@@ -32,22 +38,41 @@ pub fn create_folder_with_2_files_10_todos_8_open_in(dir: &TempDir) -> io::Resul
 }
 
 mod md_test_file_creator {
-    use std::fs::File;
-    use std::io::{self, Write};
+    use std::fs;
+    use std::io;
     use tempdir::TempDir;
 
     pub fn one_simple_file_1_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
         let file_path = dir.path().join(filename);
-        let mut f = File::create(file_path)?;
-        f.write_all(b"- [ ] A single open todo!")?;
-        f.sync_all()?;
+        fs::write(file_path, "- [ ] A single open todo!")?;
         Ok(())
     }
     pub fn one_simple_file_5_todos_4_open(dir: &TempDir, filename: &str) -> io::Result<()> {
         let file_path = dir.path().join(filename);
-        let mut f = File::create(file_path)?;
-        f.write_all(b"- [ ] A open todo!\r\n- [ ] A open todo!\r\n- [x] A closed todo!\r\n- [ ] A open todo!\r\n- [ ] A open todo!\r\n")?;
-        f.sync_all()?;
+        fs::write(file_path, "- [ ] A open todo!\r\n- [ ] A open todo!\r\n- [x] A closed todo!\r\n- [ ] A open todo!\r\n- [ ] A open todo!\r\n")?;
+        Ok(())
+    }
+
+    pub fn one_simple_file_5_todos_3_open_with_headings(
+        dir: &TempDir,
+        filename: &str,
+    ) -> io::Result<()> {
+        let file_path = dir.path().join(filename);
+        let md = "## Heading
+                # Heading2
+                - [ ] Todo with Heading
+                - [ ] A open todo!
+                
+                ### Heading Done
+                - [x] A done todo!
+                Some text that
+                does not really 000
+                matter.
+                Todos with tabs and spaces infront:
+                    - [x] A done todo!
+                    - [ ] A open todo!";
+
+        fs::write(file_path, md)?;
         Ok(())
     }
 
--- tests/md_handler_test.rs
+++ tests/md_handler_test.rs
@@ -9,6 +9,7 @@ fn test_single_file() {
     assert_eq!(todos.len(), 1);
     assert_eq!(todos[0].name, "A single open todo!");
     assert_eq!(todos[0].done, false);
+    assert_eq!(todos[0].filename, "file0.md");
     temp_folder.close().unwrap();
 }
 
@@ -32,6 +33,39 @@ fn test_single_files_multiple_todos() {
     assert_eq!(todos[0].name, "A open todo!");
     assert_eq!(todos[1].done, false);
     assert_eq!(todos[2].done, true);
+    temp_folder.close().unwrap();
+}
+
+#[test]
+fn test_single_file_with_headers() {
+    let temp_folder = common::create_md_test_file_with_5_todos_3_open_headers().unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 5);
+    assert_eq!(todos[0].first_heading, "# Heading2");
+    assert_eq!(todos[0].done, false);
+    assert_eq!(todos[0].name, "Todo with Heading");
+    assert_eq!(todos[0].filename, "file1.md");
+    assert_eq!(todos[0].line_no, 3);
+
+    assert_eq!(todos[1].first_heading, "# Heading2");
+    assert_eq!(todos[1].done, false);
+    assert_eq!(todos[1].name, "A open todo!");
+    assert_eq!(todos[1].filename, "file1.md");
+    assert_eq!(todos[1].line_no, 4);
+
+    assert_eq!(todos[2].first_heading, "### Heading Done");
+    assert_eq!(todos[2].done, true);
+    assert_eq!(todos[2].name, "A done todo!");
+    assert_eq!(todos[2].filename, "file1.md");
+    assert_eq!(todos[2].line_no, 7);
+
+    assert_eq!(todos[3].first_heading, "### Heading Done");
+    assert_eq!(todos[3].done, true);
+    assert_eq!(todos[3].name, "A done todo!");
+    assert_eq!(todos[3].filename, "file1.md");
+    assert_eq!(todos[3].line_no, 12);
+
     temp_folder.close().unwrap();
 }
 
