3df42db63484e68f8e167bfec0fb72c35712b99e
Phil Baum <philipp.baum@mailbox.org>
2022-02-18 21:30:47 +0100

    refactor integrationtests

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,59 +1,20 @@
-use std::io;
-use tempdir::TempDir;
-
-pub fn create_md_test_file_with_1_open_todo(file_amount: i32) -> io::Result<TempDir> {
-    let dir = TempDir::new("modo_integrationtests")?;
-    for elem in 0..file_amount {
-        md_test_file_creator::one_simple_file_1_open_todo(&dir, &format!("file{}.md", elem))?;
-    }
-    Ok(dir)
-}
-
-pub fn create_md_test_file_with_5_todos_4_open(file_amount: i32) -> io::Result<TempDir> {
-    let dir = TempDir::new("modo_integrationtests")?;
-    for elem in 0..file_amount {
-        md_test_file_creator::one_simple_file_5_todos_4_open(&dir, &format!("file{}.md", elem))?;
-    }
-    Ok(dir)
-}
-
-pub fn create_md_test_file_with_5_todos_3_open_headers() -> io::Result<TempDir> {
-    let dir = TempDir::new("modo_integrationtests")?;
-    md_test_file_creator::one_simple_file_5_todos_3_open_with_headings(&dir, "file1.md")?;
-    Ok(dir)
-}
-
-pub fn create_folder_with_2_files_10_todos_8_open() -> io::Result<TempDir> {
-    let dir = TempDir::new("modo_integrationtests")?;
-    md_test_file_creator::one_simple_file_5_todos_4_open(&dir, "file1.md")?;
-    md_test_file_creator::one_simple_file_5_todos_4_open(&dir, "file2.md")?;
-    Ok(dir)
-}
-
-pub fn create_folder_with_2_files_10_todos_8_open_in(dir: &TempDir) -> io::Result<TempDir> {
-    let dir = TempDir::new_in(dir, "modo_integrationtests")?;
-    md_test_file_creator::one_simple_file_5_todos_4_open(&dir, "file1.md")?;
-    md_test_file_creator::one_simple_file_5_todos_4_open(&dir, "file2.md")?;
-    Ok(dir)
-}
-
-mod md_test_file_creator {
+pub mod md_test_file_creator {
     use std::fs;
     use std::io;
     use tempdir::TempDir;
 
-    pub fn one_simple_file_1_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
+    pub fn simple_1_open_todo(dir: &TempDir, filename: &str) -> io::Result<()> {
         let file_path = dir.path().join(filename);
         fs::write(file_path, "- [ ] A single open todo!")?;
         Ok(())
     }
-    pub fn one_simple_file_5_todos_4_open(dir: &TempDir, filename: &str) -> io::Result<()> {
+    pub fn simple_5_todos_4_open(dir: &TempDir, filename: &str) -> io::Result<()> {
         let file_path = dir.path().join(filename);
         fs::write(file_path, "- [ ] A open todo!\r\n- [ ] A open todo!\r\n- [x] A closed todo!\r\n- [ ] A open todo!\r\n- [ ] A open todo!\r\n")?;
         Ok(())
     }
 
-    pub fn one_simple_file_5_todos_3_open_with_headings(
+    pub fn simple_5_todos_3_open_with_headings(
         dir: &TempDir,
         filename: &str,
     ) -> io::Result<()> {
@@ -76,5 +37,84 @@ mod md_test_file_creator {
         Ok(())
     }
 
-    // TODO: pub fn three_complex_files_with_10_todos_5_open(
+    pub fn complex_23_todos_15_open(dir: &TempDir, filename: &str) -> io::Result<()> {
+        let file_path = dir.path().join(filename);
+        let md = "# Complex note with 23 todos, 15 open
+        ## Heading2
+        - [ ] Todo with Heading
+        - [ ] A open todo!
+        
+        ```rust
+        // This is a comment, and is ignored by the compiler
+        // You can test this code by clicking the \"Run\" button over there ->
+        // or if you prefer to use your keyboard, you can use the \"Ctrl + Enter\" shortcut
+        
+        // This code is editable, feel free to hack it!
+        // You can always return to the original code by clicking the \"Reset\" button ->
+        // - [ ] Todo, should not be picked up!
+        // This is the main function
+        fn main() {
+            // Statements here are executed when the compiled binary is called
+        
+            // Print text to the console
+            println!(\"Hello World!\");
+        }
+        
+        ```
+        
+        ### Heading Done
+        - [x] A done todo!
+        Some text that
+        does not really 000
+        matter.
+        Todos with tabs and spaces infront:
+        - [x] A done todo!
+        - [ ] A open todo!
+        
+        ####
+        - Abc
+        - def
+        - gfd
+        
+        - [ ] Todo
+        - [x] todo 2
+        
+        [[temp]]
+        
+        - [ ] Todo
+        
+        [[p17_1.jpg]]
+        - [ ] Todo
+        
+        
+        ### Psipsum
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, - [ ] TODOSUM sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        
+        # new area
+        ## Work
+        - [ ] Todo 1 o
+        - [ ] Todo 2 c
+            - [ ] Sub Todo
+            - [ ] Sub Todo 2
+            - [x] Sub Todo 3 c
+        - [ ] Normal Todo again
+            - [ ] a
+                - [x] b
+                    - [ ] c
+                        - [x] d
+                            - [ ] e
+                                - [x] f
+                                    - [ ] g
+                                        - [x] h
+        
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+        
+        ## Header
+        ### Header
+        ##### Header
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
+
+        fs::write(file_path, md)?;
+        Ok(())
+    }
 }
--- tests/md_handler_test.rs
+++ tests/md_handler_test.rs
@@ -1,46 +1,65 @@
 mod common;
+use common::md_test_file_creator;
 use modo::{md_handler, todo::Todo};
+use tempdir::TempDir;
 
 #[test]
 fn test_single_file() {
-    let temp_folder = common::create_md_test_file_with_1_open_todo(1).unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_1_open_todo(&dir, "file0.md").unwrap();
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 1);
     assert_eq!(todos[0].name, "A single open todo!");
     assert_eq!(todos[0].done, false);
     assert_eq!(todos[0].filename, "file0.md");
-    temp_folder.close().unwrap();
+    dir.close().unwrap();
+}
+
+#[test]
+fn test_single_complex_file() {
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::complex_23_todos_15_open(&dir, "file0.md").unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 23);
+    dir.close().unwrap();
 }
 
 #[test]
 fn test_three_files() {
-    let temp_folder = common::create_md_test_file_with_1_open_todo(3).unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_1_open_todo(&dir, "file0.md").unwrap();
+    md_test_file_creator::simple_1_open_todo(&dir, "file1.md").unwrap();
+    md_test_file_creator::simple_1_open_todo(&dir, "file2.md").unwrap();
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 3);
     assert_eq!(todos[0].name, "A single open todo!");
     assert_eq!(todos[2].done, false);
-    temp_folder.close().unwrap();
+    dir.close().unwrap();
 }
 
 #[test]
 fn test_single_files_multiple_todos() {
-    let temp_folder = common::create_md_test_file_with_5_todos_4_open(1).unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file_123_323.md").unwrap();
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 5);
     assert_eq!(todos[0].name, "A open todo!");
     assert_eq!(todos[1].done, false);
     assert_eq!(todos[2].done, true);
-    temp_folder.close().unwrap();
+    dir.close().unwrap();
 }
 
 #[test]
 fn test_single_file_with_headers() {
-    let temp_folder = common::create_md_test_file_with_5_todos_3_open_headers().unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_3_open_with_headings(&dir, "file1.md").unwrap();
+
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 5);
     assert_eq!(todos[0].heading, "# Heading2");
     assert_eq!(todos[0].done, false);
@@ -66,37 +85,70 @@ fn test_single_file_with_headers() {
     assert_eq!(todos[3].filename, "file1.md");
     assert_eq!(todos[3].line_no, 12);
 
-    temp_folder.close().unwrap();
+    dir.close().unwrap();
 }
 
 #[test]
 fn test_multiple_files_multiple_todos() {
-    let temp_folder = common::create_md_test_file_with_5_todos_4_open(2).unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file_123_323.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "afile_123_323.md").unwrap();
+
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 10);
     //assert eq mit map/clojure
 
     assert_eq!(todos[0].name, "A open todo!");
     assert_eq!(todos[1].done, false);
     assert_eq!(todos[2].done, true);
-    temp_folder.close().unwrap();
+    dir.close().unwrap();
 }
 
 #[test]
 fn test_multiple_files_and_folders_multiple_todos() {
-    let temp_folder = common::create_folder_with_2_files_10_todos_8_open().unwrap();
-    let temp_folder_depth1 =
-        common::create_folder_with_2_files_10_todos_8_open_in(&temp_folder).unwrap();
-    let _temp_folder_depth1_2 =
-        common::create_folder_with_2_files_10_todos_8_open_in(&temp_folder).unwrap();
-    let _temp_folder_depth2 =
-        common::create_folder_with_2_files_10_todos_8_open_in(&temp_folder_depth1).unwrap();
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file2.md").unwrap();
+    let dir_depth1 = TempDir::new_in(&dir, "modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir_depth1, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir_depth1, "file2.md").unwrap();
+    let _dir_depth1_2 = TempDir::new_in(&dir, "modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth1_2, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth1_2, "file2.md").unwrap();
+    let _dir_depth2 = TempDir::new_in(&dir_depth1, "modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth2, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth2, "file2.md").unwrap();
 
     let mut todos: Vec<Todo> = vec![];
-    md_handler::load_data(temp_folder.path(), &mut todos).unwrap();
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
     assert_eq!(todos.len(), 40);
-    temp_folder.close().unwrap(); //deletes other temp folders with it
+    dir.close().unwrap();
 }
 
-// TODO: test multiple_files_multiple_folders2, very deep
+#[test]
+fn test_multiple_complex_files_and_folders_multiple_todos() {
+    let dir = TempDir::new("modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&dir, "file2.md").unwrap();
+    let _dir_depth1 = TempDir::new_in(&dir, "modo_integrationtests").unwrap();
+    md_test_file_creator::complex_23_todos_15_open(&_dir_depth1, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth1, "file2.md").unwrap();
+    let _dir_depth1_2 = TempDir::new_in(&dir, "modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth1_2, "file1.md").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth1_2, "file2.md").unwrap();
+    let _dir_depth2 = TempDir::new_in(&_dir_depth1, "modo_integrationtests").unwrap();
+    md_test_file_creator::simple_5_todos_4_open(&_dir_depth2, "file1.md").unwrap();
+    md_test_file_creator::complex_23_todos_15_open(&_dir_depth2, "file2.md").unwrap();
+    let _dir_depth3 = TempDir::new_in(&_dir_depth2, "modo_integrationtests").unwrap();
+    let _dir_depth4 = TempDir::new_in(&_dir_depth3, "modo_integrationtests").unwrap();
+    let _dir_depth5 = TempDir::new_in(&_dir_depth4, "modo_integrationtests").unwrap();
+    let _dir_depth6 = TempDir::new_in(&_dir_depth5, "modo_integrationtests").unwrap();
+    let _dir_depth7 = TempDir::new_in(&_dir_depth6, "modo_integrationtests").unwrap();
+    md_test_file_creator::complex_23_todos_15_open(&_dir_depth7, "file1.md").unwrap();
+    md_test_file_creator::complex_23_todos_15_open(&_dir_depth7, "file2.md").unwrap();
+    let mut todos: Vec<Todo> = vec![];
+    md_handler::load_data(dir.path(), &mut todos).unwrap();
+    assert_eq!(todos.len(), 122);
+    dir.close().unwrap();
+}
