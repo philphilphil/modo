c4a555f9e0a307cf43a619533c9c07ed55e93cd9
Phil Baum <philipp.baum@mailbox.org>
2022-03-21 22:10:36 +0100

    fix formatting

--- src/filter.rs
+++ src/filter.rs
@@ -7,25 +7,37 @@ pub fn filter(query: &Query, todos: &mut Vec<Todo>) {
         match clause.property {
             Property::Heading => match clause.operator {
                 Operator::Contains => todos.retain(|t| t.heading.contains(&clause.expr_string)),
-                Operator::DoesNotContain => todos.retain(|t| !t.heading.contains(&clause.expr_string)),
+                Operator::DoesNotContain => {
+                    todos.retain(|t| !t.heading.contains(&clause.expr_string))
+                }
                 Operator::Equals => todos.retain(|t| t.heading == clause.expr_string),
                 Operator::DoesNotEqual => todos.retain(|t| t.heading != clause.expr_string),
             },
             Property::Path => match clause.operator {
-                Operator::Contains => todos.retain(|t| t.filepath_as_string.contains(&clause.expr_string)),
-                Operator::DoesNotContain => todos.retain(|t| !t.filepath_as_string.contains(&clause.expr_string)),
+                Operator::Contains => {
+                    todos.retain(|t| t.filepath_as_string.contains(&clause.expr_string))
+                }
+                Operator::DoesNotContain => {
+                    todos.retain(|t| !t.filepath_as_string.contains(&clause.expr_string))
+                }
                 Operator::Equals => todos.retain(|t| t.filepath_as_string == clause.expr_string),
-                Operator::DoesNotEqual => todos.retain(|t| t.filepath_as_string != clause.expr_string),
+                Operator::DoesNotEqual => {
+                    todos.retain(|t| t.filepath_as_string != clause.expr_string)
+                }
             },
             Property::FileName => match clause.operator {
                 Operator::Contains => todos.retain(|t| t.filename.contains(&clause.expr_string)),
-                Operator::DoesNotContain => todos.retain(|t| !t.filename.contains(&clause.expr_string)),
+                Operator::DoesNotContain => {
+                    todos.retain(|t| !t.filename.contains(&clause.expr_string))
+                }
                 Operator::Equals => todos.retain(|t| t.filename == clause.expr_string),
                 Operator::DoesNotEqual => todos.retain(|t| t.filename != clause.expr_string),
             },
             Property::Name => match clause.operator {
                 Operator::Contains => todos.retain(|t| t.filter_name.contains(&clause.expr_string)),
-                Operator::DoesNotContain => todos.retain(|t| !t.filter_name.contains(&clause.expr_string)),
+                Operator::DoesNotContain => {
+                    todos.retain(|t| !t.filter_name.contains(&clause.expr_string))
+                }
                 Operator::Equals => todos.retain(|t| t.filter_name == clause.expr_string),
                 Operator::DoesNotEqual => todos.retain(|t| t.filter_name != clause.expr_string),
             },
