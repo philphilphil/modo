4cc749d0908e6b8b55001c8f44450476e3cb04ef
Phil Baum <philipp.baum@mailbox.org>
2022-05-16 22:48:13 +0200

    more termion

--- src/ui.rs
+++ src/ui.rs
@@ -1,12 +1,13 @@
 use crate::Todo;
 use anyhow::Result;
 use modo::md_writer;
-use std::io::{stdin, stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
 use std::path::Path;
+use termion::color::Fg;
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
-use termion::{color, style};
+use termion::{color, cursor, style};
 enum UserAction {
     Navigation,
     Quit,
@@ -25,12 +26,7 @@ pub fn draw_ui(query: &str, path: &Path) -> Result<()> {
         loop {
             write!(stdout, "{}{}", termion::clear::All, termion::cursor::Hide).unwrap();
             stdout.flush().unwrap();
-            println!(
-                "{}Query: {}{}",
-                termion::cursor::Goto(1, 1),
-                query,
-                selected_todo_index
-            );
+            println!("{}Query: {}", termion::cursor::Goto(1, 1), query);
 
             for (i, todo) in todos.iter().enumerate() {
                 write!(stdout, "{}", termion::cursor::Goto(1, i as u16 + 3)).unwrap();
@@ -46,7 +42,7 @@ pub fn draw_ui(query: &str, path: &Path) -> Result<()> {
             match key_result {
                 UserAction::Navigation => continue,
                 UserAction::Details => {
-                    draw_todo_details(&todos[selected_todo_index as usize]);
+                    draw_todo_details(&todos[selected_todo_index as usize], &mut stdout);
                     break;
                 }
                 UserAction::Quit => {
@@ -59,41 +55,72 @@ pub fn draw_ui(query: &str, path: &Path) -> Result<()> {
     }
 }
 
-fn draw_todo_details(todo: &Todo) {
-    //Todo with additonal screen thing?
-    // clear();
-    // addstr(&format!("Name: {}\n", todo.name));
-    // addstr(&format!("Status: {}\n", todo.done));
-    // addstr(&format!("Heading: {}\n", todo.heading));
-    // addstr(&format!("Filename: {}\n", todo.filename));
-    // addstr(&format!("Line Number: {}\n", todo.line_no));
-    // match getch() {
-    //     120 | 10 => {
-    //         // x / enter
-    //         md_writer::toggle_todo(todo).unwrap();
-    //     }
-    //     _ => {}
-    // }
+fn draw_todo_details(todo: &Todo, stdout: &mut Stdout) {
+    // Todo with additonal screen thin
+    write!(stdout, "{}{}", termion::clear::All, termion::cursor::Hide).unwrap();
+    stdout.flush().unwrap();
+    draw_todo_detail_line("Name", &todo.name, 1);
+
+    if todo.done {
+        draw_todo_detail_line_color("Status", "Done", color::Fg(color::Green), 2);
+    } else {
+        draw_todo_detail_line_color("Status", "Open", color::Fg(color::Red), 2);
+    }
+
+    draw_todo_detail_line("Heading", &todo.heading, 3);
+    draw_todo_detail_line("Filename", &todo.filename, 4);
+    draw_todo_detail_line("Lineno", &todo.line_no.to_string(), 5);
+
+    if let Some(c) = stdin().keys().next() {
+        if let Key::Char('x') = c.unwrap() {
+            md_writer::toggle_todo(todo).unwrap();
+        }
+    }
+}
+
+fn draw_todo_detail_line(attribute: &str, value: &str, line_no: u16) {
+    println!(
+        "{}{}{}: {}{}",
+        cursor::Goto(1, line_no),
+        style::Bold,
+        attribute,
+        style::Reset,
+        value
+    );
+}
+fn draw_todo_detail_line_color<K: termion::color::Color>(
+    attribute: &str,
+    value: &str,
+    color: Fg<K>,
+    line_no: u16,
+) {
+    println!(
+        "{}{}{}:{} {}{}{}",
+        cursor::Goto(1, line_no),
+        style::Bold,
+        attribute,
+        style::Reset,
+        color,
+        value,
+        style::Reset,
+    );
 }
 
 fn listen_nav_key(selected_todo_index: &mut usize, todos: &[Todo]) -> UserAction {
     if let Some(c) = stdin().keys().next() {
         match c.unwrap() {
             Key::Char('j') | Key::Down => {
-                // j / arrow down
                 if *selected_todo_index != todos.len() - 1 {
                     *selected_todo_index += 1;
                 }
             }
             Key::Char('k') | Key::Up => {
-                // k / arrow up
                 if *selected_todo_index != 0 {
                     *selected_todo_index -= 1
                 }
             }
             Key::Char('q') => return UserAction::Quit, // q
             Key::Char('x') => {
-                // x
                 md_writer::toggle_todo(&todos[*selected_todo_index]).unwrap();
                 return UserAction::Reload;
             }
